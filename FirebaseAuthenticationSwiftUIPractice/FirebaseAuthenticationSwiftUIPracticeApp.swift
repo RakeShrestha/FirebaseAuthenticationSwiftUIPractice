ed05d6a55fbc93d2a4ea4308fa0c55cf2444666b
RakeShrestha <orakeshstha7@gmail.com>
2025-01-11 10:53:15 +0545

    Initial Commit

---
+++ FirebaseAuthenticationSwiftUIPractice/FirebaseAuthenticationSwiftUIPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseAuthenticationSwiftUIPracticeApp.swift
+//  FirebaseAuthenticationSwiftUIPractice
+//
+//  Created by Rakesh Shrestha on 11/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseAuthenticationSwiftUIPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
