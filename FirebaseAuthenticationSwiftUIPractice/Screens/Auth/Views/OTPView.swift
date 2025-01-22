839c8f4cb6fc4049bb45e270aa7b2e65d7e60686
RakeShrestha <orakeshstha7@gmail.com>
2025-01-22 11:06:30 +0545

    Refactored code for OTP Screen

--- FirebaseAuthenticationSwiftUIPractice/Screens/Auth/Views/OTPView.swift
+++ FirebaseAuthenticationSwiftUIPractice/Screens/Auth/Views/OTPView.swift
@@ -10,64 +10,26 @@ import SwiftUI
 struct OTPView: View {
     let receivedOTP = "1234"
     @State private var enteredOTP: String = ""
+    @State private var didOTPMatched : Bool = true
     
     var body: some View {
         ZStack {
             VStack {
                 VStack(alignment: .leading, spacing: 20) {
-                    Text("Verification Code")
-                        .font(.title)
-                        .fontWeight(.bold)
-                        .foregroundStyle(.white)
-                    Text("We have sent the verification code to your email address")
-                        .font(.headline)
-                        .fontWeight(.bold)
-                        .foregroundStyle(.white.opacity(0.7))
-                    HStack(alignment: .center) {
-                        Spacer()
-                        OTPTextField(numberOfPinFields: 4, enteredOTP: $enteredOTP)
-                        Spacer()
+                    headerView
+                    
+                   otpFieldView
+                    
+                    if !didOTPMatched {
+                        errorMessageView
                     }
+                    
                     Spacer()
                     
-                    Button {
-                            if verifyOTP() {
-                                // OTP matches
-                                print("OTP Verified Successfully!")
-                            } else {
-                                // OTP does not match
-                                print("Incorrect OTP")
-                            }
-                    } label: {
-                        HStack {
-                            Text("Confirm")
-                                .foregroundStyle(Color.white)
-                                .padding(.vertical)
-                            
-                            Image("arrowRight")
-                                .resizable()
-                                .frame(width: 24, height: 24)
-                                .padding(.trailing, 4)
-                        }
-                    }
-                        
-                        .frame(maxWidth: .infinity, alignment: .center)
-                        .background(enteredOTP.count == 4 ? Capsule().fill(Color.init(hex: "F97316")) : Capsule().fill(Color.gray.opacity(0.2)))
-                    .padding(.vertical, 12)
-                    .disabled(enteredOTP.count != 4)
+                    confirmButtonView
                 }
                 
-                HStack {
-                    Text("Didn't receive code?")
-                        .font(.subheadline)
-                        .foregroundStyle(.white)
-                    Button {
-                        
-                    } label: {
-                        Text("Request again")
-                            .foregroundStyle(Color.init(hex: "F97316"))
-                    }
-                }
+                footerView
             }
             .padding(.all, 32)
         }
@@ -76,6 +38,81 @@ struct OTPView: View {
     
     func verifyOTP() -> Bool {
         return enteredOTP == receivedOTP
+    }
+    
+    private var headerView : some View {
+        VStack(alignment: .leading) {
+            Text("Verification Code")
+                .font(.title)
+                .fontWeight(.bold)
+                .foregroundStyle(.white)
+            Text("We have sent the verification code to your email address")
+                .font(.headline)
+                .fontWeight(.bold)
+                .foregroundStyle(.white.opacity(0.7))
+        }
+    }
+    
+    private var otpFieldView : some View {
+        HStack(alignment: .center) {
+            Spacer()
+            OTPTextField(numberOfPinFields: 4, enteredOTP: $enteredOTP)
+            Spacer()
+        }
+    }
+    
+    private var confirmButtonView : some View {
+        Button {
+                if verifyOTP() {
+                    // OTP matches
+                    print("OTP Verified Successfully!")
+                } else {
+                    // OTP does not match
+                    withAnimation {
+                        didOTPMatched = false
+                    }
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 2, execute: {
+                        withAnimation {
+                            didOTPMatched = true
+                        }
+                    })
+                }
+        } label: {
+            HStack {
+                Text("Confirm")
+                    .foregroundStyle(Color.white)
+                    .padding(.vertical)
+                
+                Image("arrowRight")
+                    .resizable()
+                    .frame(width: 24, height: 24)
+                    .padding(.trailing, 4)
+            }
+        }
+            
+            .frame(maxWidth: .infinity, alignment: .center)
+            .background(enteredOTP.count == 4 ? Capsule().fill(Color.init(hex: "F97316")) : Capsule().fill(Color.gray.opacity(0.2)))
+        .padding(.vertical, 12)
+        .disabled(enteredOTP.count != 4)
+    }
+    
+    private var footerView : some View {
+        HStack {
+            Text("Didn't receive code?")
+                .font(.subheadline)
+                .foregroundStyle(.white)
+            Button {
+                
+            } label: {
+                Text("Request again")
+                    .foregroundStyle(Color.init(hex: "F97316"))
+            }
+        }
+        .padding(.bottom, 12)
+    }
+    
+    private var errorMessageView : some View {
+            ErrorMessage(message: "ERROR: OTP does not match")
     }
     
 }
